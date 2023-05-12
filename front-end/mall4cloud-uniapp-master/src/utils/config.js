7e1e0d2043c3e879500fd97f716bb992e92db0c5
yxf <2676997212@qq.com>
2023-05-12 09:37:02 +0800

    统一文档的ip

--- front-end/mall4cloud-uniapp-master/src/utils/config.js
+++ front-end/mall4cloud-uniapp-master/src/utils/config.js
@@ -8,8 +8,8 @@ var mpAppId = 'wx42bbe857570c5b40'
 
 // 本地运行环境 ->  地址修改为本机IP; 端口与 manifest.json 中 h5 -> devServer -> port 一致;
 // 运行后浏览器使用 "http://本机IP+端口" 访问
-var domain = 'http://192.168.1.11:8000' // 统一接口域名，测试环境
-var wsDomain = 'ws://192.168.1.11:8000' // 统一接口域名，测试环境
+var domain = 'http://192.168.1.46:8000' // 统一接口域名，测试环境
+var wsDomain = 'ws://192.168.1.46:8000' // 统一接口域名，测试环境
 
 var picDomain = 'http://img-test.gz-yami.com/' // 图片域名
 
