ab7f55270a018a1f34d3cb95edd361cd3fd97981
yxf <2676997212@qq.com>
2023-09-18 11:08:54 +0800

    更新es配置

--- mall4cloud-search/src/main/java/com/mall4j/cloud/search/config/ElasticConfig.java
+++ mall4cloud-search/src/main/java/com/mall4j/cloud/search/config/ElasticConfig.java
@@ -3,6 +3,7 @@ package com.mall4j.cloud.search.config;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticConfig {
 
+    @Value("${elastic.address}")
+    private String address;
+
+    @Value("${elastic.port}")
+    private Integer port;
     @Bean
     public RestHighLevelClient restHighLevelClient() {
         return new RestHighLevelClient(
