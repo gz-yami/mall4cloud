9eb3620d13f655cb49ad9fe02693bfba1a0a7783
yxf <2676997212@qq.com>
2024-03-28 10:51:13 +0800

    更改seata配置方式

--- doc/中间件docker-compse一键安装/mysql/initdb/mall4cloud_seata.sql
+++ doc/中间件docker-compse一键安装/mysql/initdb/mall4cloud_seata.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v13.1.1 (64 bit)
-MySQL - 8.0.25 : Database - mall4cloud_seata
+MySQL - 8.0.33 : Database - mall4cloud_seata
 *********************************************************************
 */
 
@@ -12,68 +12,86 @@ MySQL - 8.0.25 : Database - mall4cloud_seata
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`mall4cloud_seata` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mall4cloud_seata` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `mall4cloud_seata`;
 
 /*Table structure for table `branch_table` */
 
-DROP TABLE IF EXISTS `branch_table`;
-
-CREATE TABLE `branch_table` (
-  `branch_id` bigint NOT NULL,
-  `xid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `transaction_id` bigint DEFAULT NULL,
-  `resource_group_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `resource_id` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `branch_type` varchar(8) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `status` tinyint DEFAULT NULL,
-  `client_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `application_data` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `gmt_create` datetime(6) DEFAULT NULL,
-  `gmt_modified` datetime(6) DEFAULT NULL,
-  PRIMARY KEY (`branch_id`) USING BTREE,
-  KEY `idx_xid` (`xid`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-/*Table structure for table `global_table` */
-
 DROP TABLE IF EXISTS `global_table`;
+CREATE TABLE IF NOT EXISTS `global_table`
+(
+    `xid`                       VARCHAR(128) NOT NULL,
+    `transaction_id`            BIGINT,
+    `status`                    TINYINT      NOT NULL,
+    `application_id`            VARCHAR(32),
+    `transaction_service_group` VARCHAR(32),
+    `transaction_name`          VARCHAR(128),
+    `timeout`                   INT,
+    `begin_time`                BIGINT,
+    `application_data`          VARCHAR(2000),
+    `gmt_create`                DATETIME,
+    `gmt_modified`              DATETIME,
+    PRIMARY KEY (`xid`),
+    KEY `idx_status_gmt_modified` (`status` , `gmt_modified`),
+    KEY `idx_transaction_id` (`transaction_id`)
+    ) ENGINE = INNODB
+    DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `global_table` (
-  `xid` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `transaction_id` bigint DEFAULT NULL,
-  `status` tinyint NOT NULL,
-  `application_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `transaction_service_group` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `transaction_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `timeout` int DEFAULT NULL,
-  `begin_time` bigint DEFAULT NULL,
-  `application_data` varchar(2000) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `gmt_create` datetime DEFAULT NULL,
-  `gmt_modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`xid`) USING BTREE,
-  KEY `idx_gmt_modified_status` (`gmt_modified`,`status`) USING BTREE,
-  KEY `idx_transaction_id` (`transaction_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+-- the table to store BranchSession data
+DROP TABLE IF EXISTS `branch_table`;
+CREATE TABLE IF NOT EXISTS `branch_table`
+(
+    `branch_id`         BIGINT       NOT NULL,
+    `xid`               VARCHAR(128) NOT NULL,
+    `transaction_id`    BIGINT,
+    `resource_group_id` VARCHAR(32),
+    `resource_id`       VARCHAR(256),
+    `branch_type`       VARCHAR(8),
+    `status`            TINYINT,
+    `client_id`         VARCHAR(64),
+    `application_data`  VARCHAR(2000),
+    `gmt_create`        DATETIME(6),
+    `gmt_modified`      DATETIME(6),
+    PRIMARY KEY (`branch_id`),
+    KEY `idx_xid` (`xid`)
+    ) ENGINE = INNODB
+    DEFAULT CHARSET = utf8mb4;
 
-/*Table structure for table `lock_table` */
-
+-- the table to store lock data
 DROP TABLE IF EXISTS `lock_table`;
+CREATE TABLE `lock_table`
+(
+    `row_key`        VARCHAR(128) NOT NULL,
+    `xid`            VARCHAR(128),
+    `transaction_id` BIGINT,
+    `branch_id`      BIGINT       NOT NULL,
+    `resource_id`    VARCHAR(256),
+    `table_name`     VARCHAR(32),
+    `pk`             VARCHAR(36),
+    `status`         TINYINT      NOT NULL DEFAULT '0' COMMENT '0:locked ,1:rollbacking',
+    `gmt_create`     DATETIME,
+    `gmt_modified`   DATETIME,
+    PRIMARY KEY (`row_key`),
+    KEY `idx_status` (`status`),
+    KEY `idx_branch_id` (`branch_id`),
+    KEY `idx_xid` (`xid`)
+) ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4;
 
-CREATE TABLE `lock_table` (
-  `row_key` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `xid` varchar(96) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `transaction_id` bigint DEFAULT NULL,
-  `branch_id` bigint NOT NULL,
-  `resource_id` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `table_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `pk` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `gmt_create` datetime DEFAULT NULL,
-  `gmt_modified` datetime DEFAULT NULL,
-  PRIMARY KEY (`row_key`) USING BTREE,
-  KEY `idx_branch_id` (`branch_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE IF NOT EXISTS `distributed_lock`
+(
+    `lock_key`       CHAR(20) NOT NULL,
+    `lock_value`     VARCHAR(20) NOT NULL,
+    `expire`         BIGINT,
+    PRIMARY KEY (`lock_key`)
+    ) ENGINE = INNODB
+    DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire) VALUES ('AsyncCommitting', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire) VALUES ('RetryCommitting', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire) VALUES ('RetryRollbacking', ' ', 0);
+INSERT INTO `distributed_lock` (lock_key, lock_value, expire) VALUES ('TxTimeoutCheck', ' ', 0);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
